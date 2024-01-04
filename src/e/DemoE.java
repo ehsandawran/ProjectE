3ada7b9b1537ecf3bc267539e73ac7a6dee759a9
ehsan <ehsan@ehsans-air.hsd1.tn.comcast.net>
2024-01-03 21:19:00 -0500

    last commit of the day

---
+++ src/e/DemoE.java
@@ -0,0 +1,14 @@
+package e;
+
+public class DemoE {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("This is class E");
+		
+		System.out.println("I'm pretty sure I got this...");
+		System.out.println("of course I don't forget");
+		
+	}
+
+}
